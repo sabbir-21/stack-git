d7644dadacd62292c887e0341bf7ef82d7451855
Sabbir <mdsabbirahmed25@gmail.com>
2020-12-17 17:43:38 +0600

    added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Edited with VS");
+        greeting("Sabbir bro");
+    }
+    public static void greeting(String name) {
+        System.out.println("welcome" + name);
     }
 }
